c68ff54c86509dae543f86ee2c6371acf7bb4d72
Дмитрий <kriukov@pos-credit.ru>
2024-06-25 00:19:57 +0300

    add read json

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from src.utils import open_read_json
+
+
 class Category:
     """класс для категорий"""
 
@@ -35,8 +38,26 @@ class Product:
         self.quantity = quantity
 
 
-emp_1 = Category("фрукты", "отечественные", ["яблоки", "слива", "груша"])
+# emp_1 = Category("фрукты", "отечественные", ["яблоки", "слива", "груша"])
 emp_2 = Product("яблоки", "отечественные", 15.5, 55)
-emp_3 = Category.number_of_categories
-emp_4 = Category.number_of_unique_products
+# emp_3 = Category.number_of_categories
+# emp_4 = Category.number_of_unique_products
 
+file_path = "../data/products.json"
+result = open_read_json(file_path)
+categories = []
+for category_data in result:
+    products = []
+    for product_data in category_data["products"]:
+        product = Product(
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"],
+        )
+        products.append(product)
+    category = Category(name=category_data["name"], description=category_data["description"], goods=products)
+    categories.append(category)
+
+print(Category.number_of_categories)
+print(Category.number_of_unique_products)
